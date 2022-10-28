9ca7b8c8724e3460cc1de5c308210762f4320f53
hannarim-23 <80169404+hannarim-23@users.noreply.github.com>
2022-10-28 06:47:45 +0300

    refactor: change class according to rule 2.1 BEM

--- app.js
+++ app.js
@@ -43,9 +43,9 @@ var createNewTaskElement=function(taskString){
     editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.classList.add("btn", "edit");
+    editButton.classList.add("btn", "btn-edit");
 
-    deleteButton.classList.add("btn", "delete");
+    deleteButton.classList.add("btn", "btn-delete");
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.classList.add("btn-img");
     deleteButton.appendChild(deleteButtonImg);
@@ -87,7 +87,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".btn-edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .edit-mode
     if(containsClass){
@@ -159,8 +159,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.btn-edit");
+    var deleteButton=taskListItem.querySelector("button.btn-delete");
 
 
     //Bind editTask to edit button.
